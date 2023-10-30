17dbc1152afdef214202299031901fc4a79d71e2
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2023-10-29 19:41:57 -0700

    Added expressjs

---
+++ src/server/index.ts
@@ -0,0 +1,14 @@
+// server/index.ts
+
+import express from 'express';
+
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express server!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
